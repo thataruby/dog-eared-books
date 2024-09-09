2656ec636c7bf74b0f5efb547cacb004081b3b84
thataruby <thata@dfi.id>
2024-09-09 15:08:32 +0700

    updated urls

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+            'application_name': 'dog-eared-books',
+            'class': 'PBD KKI',
+            'name': 'Athazahra Nabila Ruby',
+    }
+    return render(request, "main.html", context)
